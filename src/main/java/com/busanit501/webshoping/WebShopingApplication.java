d2c5629e8ca340ce5735a00d6eba500fed6f55bd
yoojiseong <yujiseong588@gmail.com>
2025-07-16 23:50:21 +0900

    first commit

---
+++ src/main/java/com/busanit501/webshoping/WebShopingApplication.java
@@ -0,0 +1,13 @@
+package com.busanit501.webshoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebShopingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/busanit501/webshoping/WebShopingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit501.webshoping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
